a7071345efb073b7a06ac94b5875265bc0d5260c
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-07-26 20:33:13 +0500

    Add colors to bulma and test them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "./css/App.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App p-2 container">
+      <div className="notification is-grey is-rounded">
+        App
+      </div>
     </div>
   );
 }
