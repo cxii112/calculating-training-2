3d54b5cf261115b8c12f59b33c7bb71057c8903f
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-08-06 23:27:24 +0500

    Change operation state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import {
+  useEffect,
+  useState
+} from 'react';
 import Operation from './additional/Operation';
 import ActionButton from './components/ActionButton';
-import OperatorPeeker from './components/OperationPicker';
+import OperationPicker from './components/OperationPicker';
 import './css/App.css';
 
 function App() {
   const [operation, setOperation] = useState<Operation>(Operation.SUMM);
+  useEffect(() => {
+    setOperation(Operation.SUMM);
+  }, []);
   return (
     <div className='App'>
       <section className='container p-2 is-flex is-flex-direction-column is-justify-content-center is-align-content-center fixedWidth'>
         <div className='notification is-grey is-rounded'>
-          <OperatorPeeker
-            operationPickerCallback={
-              (newOperation: Operation) => setOperation(newOperation)
-            }/>
+          <OperationPicker
+            operationPickerCallback={setOperation}/>
           <ActionButton
             classes={['button',
               'is-primary',
--- src/components/OperationPicker.tsx
+++ src/components/OperationPicker.tsx
@@ -1,37 +1,69 @@
 import createIonIcon from '../additional/createIonIcon';
+import Operation from '../additional/Operation';
+import { removeClassByClass } from '../additional/removeClassByClass';
+import { toggleClassById } from '../additional/toggleClassById';
 interface OperationPickerProps {
   operationPickerCallback: Function
 }
 
 function OperationPicker(props: OperationPickerProps) {
-  const buttonClasses = [
-    'is-primary',
-    'is-outlined'
-  ];
   const plusSign = createIonIcon('add-outline');
   const minusSign = createIonIcon('remove-outline');
   const multiplySign = createIonIcon('close-outline');
   const divideSign = '÷';
+  const tabsClasses = [
+    'tab'
+  ];
+  const clickHandler = (currentTabId: string,
+    newOperation: Operation) => {
+    removeClassByClass('tab', 'is-active');
+    toggleClassById(currentTabId, 'is-active');
+    props.operationPickerCallback(newOperation);
+  };
   return (
     <div className='OperationPicker py-4'>
       <div className='tabs is-centered is-boxed is-fullwidth is-primary is-outlined'>
         <ul>
-          <li className='is-active'>
+          <li
+            id='summ-tab'
+            className={tabsClasses.join(' ') + ' ' + 'is-active'}
+            onClick={event => {
+              clickHandler(event.currentTarget.id,
+                Operation.SUMM)
+            }}>
             <a>
               <span>{plusSign}</span>
             </a>
           </li>
-          <li className=''>
+          <li
+            id='subtract-tab'
+            className={tabsClasses.join(' ')}
+            onClick={event => {
+              clickHandler(event.currentTarget.id,
+                Operation.SUBTRACT)
+            }}>
             <a>
               <span>{minusSign}</span>
             </a>
           </li>
-          <li className=''>
+          <li
+            id='multiply-tab'
+            className={tabsClasses.join(' ')}
+            onClick={event => {
+              clickHandler(event.currentTarget.id,
+                Operation.MULTIPLY)
+            }}>
             <a>
               <span>{multiplySign}</span>
             </a>
           </li>
-          <li className=''>
+          <li
+            id='divide-tab'
+            className={tabsClasses.join(' ')}
+            onClick={event => {
+              clickHandler(event.currentTarget.id,
+                Operation.DIVIDE)
+            }}>
             <a>
               <span>{divideSign}</span>
             </a>
